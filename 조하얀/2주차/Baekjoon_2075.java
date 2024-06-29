31bd9928d8e488aafddbef7d4f9f01ec2c07cc52
cwhite723 <c_white723@naver.com>
2024-06-29 17:32:27 +0900

    2주차 풀이 완료

---
+++ 조하얀/2주차/Baekjoon_2075.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Baekjoon_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringTokenizer st;
+        for (int i = 0; i < number; i++) {
+            st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                pq.offer(Integer.parseInt(st.nextToken()));
+                if (pq.size() > number) pq.poll();
+            }
+        }
+
+        System.out.println(pq.poll());
+    }
+}
